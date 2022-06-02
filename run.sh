caceaa9f22f1340d62159abc4236a0991c1561aa
James Alexander <jalexander@leafsoftwaresolutions.com>
2022-06-02 08:22:26 -0400

    - Updating run command for cron

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/jalexander/.virtualenvs/toggl-round/bin/postactivate
-/home/jalexander/.virtualenvs/toggl-round/bin/python /home/jalexander/src/toggl-round/togglRound.py
+source /root/.local/share/virtualenvs/toggl-round-ixjOdO2R/bin/postactivate
+/root/.local/share/virtualenvs/toggl-round-ixjOdO2R/bin/python /root/src/toggl-round/togglRound.py
