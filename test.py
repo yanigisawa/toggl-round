29bc2ca67c6d22b2d0ac09351f0f8e44a7800238
James Alexander <jalexander@leafsoftwaresolutions.com>
2022-06-02 08:01:40 -0400

    - Dependency updates for Python 3.9

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from togglRound import roundToQuarterHour 
+import togglRound as tr
 from datetime import datetime
 import pytz
 
@@ -8,55 +8,118 @@ utc = pytz.utc
 def test_07MinutesRoundsDown():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 7, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_08MinutesRoundsUp():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 8, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, 15, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_22MinutesRoundsDown():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 22, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, 15, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_23MinutesRoundsUp():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 23, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, 30, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_37MinutesRoundsDown():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 37, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, 30, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_38MinutesRoundsUp():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 38, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, 45, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_52MinutesRoundsDown():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 52, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour, 45, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_53MinutesRoundsUp():
     tmpDt = datetime(now.year, now.month, now.day, now.hour, 53, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day, now.hour + 1, 0, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_53After23HoursRoundsToNextDay():
     tmpDt = datetime(now.year, now.month, now.day, 23, 53, tzinfo=utc)
     expected = datetime(now.year, now.month, now.day + 1, 0, 0, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
 
 def test_53After23HoursAtEndOfMonthRoundsToNextMonth():
     tmpDt = datetime(now.year, 1, 31, 23, 53, tzinfo=utc)
     expected = datetime(now.year, 2, 1, 0, 0, tzinfo=utc)
-    assert(roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+    assert(tr.roundToQuarterHour(tmpDt).isoformat() == expected.isoformat())
+
+def test_lessThanEightHoursCreatesAdminTime():
+    start = datetime(now.year, 1, 31, 7, 0, tzinfo=utc)
+    end = datetime(now.year, 1, 31, 12, 0, tzinfo=utc)
+    entry = tr.TimeEntry(start=start.isoformat(), stop=end.isoformat(), duration=18000)
+
+    expected_admin_start = datetime(now.year, 1, 31, 12, 0, tzinfo=utc)
+    expected_admin_stop = datetime(now.year, 1, 31, 15, 0, tzinfo=utc)
+    admin_entry = tr.fillWithAdminTime([entry])[0]
+    assert(admin_entry.start == expected_admin_start)
+    assert(admin_entry.duration == 10800)
+    assert(admin_entry.stop == expected_admin_stop)
+
+def test_lessThanEightHoursCreatesAdminTimeMultiDay():
+    start = datetime(now.year, 1, 31, 7, 0, tzinfo=utc)
+    end = datetime(now.year, 1, 31, 12, 0, tzinfo=utc)
+    entries = []
+    entries.append(tr.TimeEntry(start=start.isoformat(), stop=end.isoformat(), duration=18000))
+
+    start = datetime(now.year, 2, 1, 7, 0, tzinfo=utc)
+    end = datetime(now.year, 2, 1, 12, 0, tzinfo=utc)
+    entries.append(tr.TimeEntry(start=start.isoformat(), stop=end.isoformat(), duration=18000))
+
+    expected = [
+        tr.TimeEntry(
+            start=datetime(now.year, 1, 31, 12, 0, tzinfo=utc).isoformat(),
+            stop=datetime(now.year, 1, 31, 15, 0, tzinfo=utc).isoformat(),
+            duration=10800),
+        tr.TimeEntry(
+            start=datetime(now.year, 2, 1, 12, 0, tzinfo=utc).isoformat(),
+            stop=datetime(now.year, 2, 1, 15, 0, tzinfo=utc).isoformat(),
+            duration=10800),
+    ]
+
+    actual_entries = tr.fillWithAdminTime(entries)
+    for actual, expected in zip(actual_entries, expected):
+        assert(actual.start == expected.start)
+        assert(actual.duration == expected.duration)
+        assert(actual.stop == expected.stop)
 
 
+def test_greaterThanEightHoursHasNoAdminTime():
+    start = datetime(now.year, 1, 31, 7, 0, tzinfo=utc)
+    end = datetime(now.year, 1, 31, 16, 0, tzinfo=utc)
+    entries = []
+    diff = end - start
+    entries.append(tr.TimeEntry(start=start.isoformat(), stop=end.isoformat(), duration=diff.total_seconds()))
 
+    actual_entries = tr.fillWithAdminTime(entries)
+    assert(len(actual_entries) == 0)
 
+def test_runningTimerDoesNotCreateAdminTime():
+    start = datetime(now.year, 1, 31, 7, 0, tzinfo=utc)
+    entries = []
+    entries.append(tr.TimeEntry(start=start.isoformat()))
 
+    actual_entries = tr.fillWithAdminTime(entries)
+    assert(len(actual_entries) == 0)
 
+def test_negativeDurationDoesNotCreateAdminTime():
+    start = datetime(now.year, 1, 31, 7, 0, tzinfo=utc)
+    entries = []
+    entries.append(tr.TimeEntry(start=start.isoformat(), duration=-1544631140))
+
+    actual_entries = tr.fillWithAdminTime(entries)
+    assert(len(actual_entries) == 0)
+
+# Need to go back two weeks and ensure that no more than 40 hours per week exist
+# before adding Admin time
--- togglRound.py
+++ togglRound.py
@@ -2,7 +2,7 @@ import json
 import requests
 import os
 from dateutil.parser import parse
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import pytz
 
 class TimeEntryEncoder(json.JSONEncoder):
@@ -17,15 +17,14 @@ def truncateSeconds(dt):
     return datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute, tzinfo = dt.tzinfo)
 
 def roundToQuarterHour(dt):
-    roundTo = int((dt.minute + 7.5) / 15) * 15 
+    roundTo = int((dt.minute + 7.5) / 15) * 15
     m = roundTo - dt.minute
-    return dt + timedelta(minutes = m) 
+    return dt + timedelta(minutes = m)
 
 class TimeEntry:
-    def __init__(self, description = None, tags = None, duration = None,
-            start = None, stop = None, duronly = None, pid = None,
-            billable = None, guid = None, at = None, wid = None, 
-            id = None, uid = None):
+    def __init__(self, start = None, stop = None, duronly = None, pid = None,
+            billable = None, guid = None, at = None, wid = None,
+            id = None, uid = None, description=None, duration=None, tags=None):
         self.description = description
         self.tags = tags
 
@@ -35,8 +34,13 @@ class TimeEntry:
         if stop != None:
             self.stop = roundToQuarterHour(truncateSeconds(parse(stop)))
 
-        if start != None and stop != None:
+        if duration != None and duration > 0:
+            self.duration = duration
+        elif start != None and stop != None:
             self.duration = (self.stop - self.start).seconds
+        else:
+            self.duration = 28800
+
         self.duronly = duronly
         self.pid = pid
         self.billable = billable
@@ -47,13 +51,14 @@ class TimeEntry:
         self.uid = uid
 
     def __repr__(self):
-        return "{0} - {1} - {2}".format(self.start, self.stop, self.duration)
+        # return "{0} - {1} - {2}".format(self.start, getattr(self, 'stop', None), self.duration)
+        return "{0}".format(self.__dict__)
 
 def getHeaders():
     api_key = os.environ.get("TOGGL_API_KEY")
     if not api_key:
         print("'TOGGL_API_KEY' environment variable not set. Please set this variable to continue")
-        return 
+        return
 
     base64Token = "{0}:api_token".format(api_key).encode("base64").rstrip()
     return { "Authorization" : "Basic " + base64Token }
@@ -75,11 +80,55 @@ def getTimeEntries(startDate = None, endDate = None):
 
     entries = []
     print("Found {0} item(s)".format(len(resp.json())))
+    print("JSON: {0}".format(resp.json()))
     for e in resp.json():
         entry = TimeEntry(**e)
         entries.append(entry)
 
     return entries
+
+def getLastTimeForDay(date_string, entries):
+    latest_date = datetime(1987, 1, 31, 12, 0, tzinfo=pytz.utc)
+    dt = parse(date_string)
+    day_entries = sorted([e for e in entries if e.start.date() == dt.date()], key=lambda x: x.start)
+    for e in day_entries:
+        if hasattr(e, 'stop') and e.stop > latest_date:
+            latest_date = e.stop
+    if latest_date == date.min:
+        return None
+    return latest_date
+
+def get_time_per_day(entries):
+    time_per_day = {}
+    for e in entries:
+        date_key = e.start.date().isoformat()
+        if date_key not in time_per_day.keys():
+            time_per_day[date_key] = e.duration
+            continue
+        time_per_day[date_key] += e.duration
+    return time_per_day
+
+def fillWithAdminTime(entries):
+    now = datetime.now()
+    utc = pytz.utc
+    # import pdb; pdb.set_trace()
+    time_per_day = get_time_per_day(entries)
+
+    admin_entries = []
+    eight_hours_in_seconds = 28800
+    for date, duration in time_per_day.items():
+        if duration < eight_hours_in_seconds:
+            admin_duration = eight_hours_in_seconds - duration
+            start = getLastTimeForDay(date, entries)
+            te = TimeEntry(
+                start=start.isoformat(),
+                stop=roundToQuarterHour(start + timedelta(seconds=admin_duration)).isoformat(),
+                wid=876389, pid=12780480, description='Admin')
+
+            admin_entries.append(te)
+
+    print('Would create Admin: {0}'.format(admin_entries))
+    return admin_entries
 
 def updateEntries(entries):
     headers = getHeaders()
@@ -98,7 +147,15 @@ def main():
     entries = getTimeEntries(startDate = datetime(yesterday.year,
         yesterday.month, yesterday.day, tzinfo = utc))
     updateEntries(entries)
+    # TODO - have not tested the Admin Time feature
+    # admin_entries = fillWithAdminTime(entries)
+    # updateEntries(admin_entries)
     print("Update Complete")
 
 if __name__ == "__main__":
     main()
+
+    # entries = getTimeEntries()
+    # fillWithAdminTime(entries)
+    # for e in entries:
+    #     print(e)
